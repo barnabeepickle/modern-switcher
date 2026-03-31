0bec6cddee899103288056351568538c391f3a85
barnabeepickle <picklebarnabee@gmail.com>
2026-03-31 11:47:52 -0400

    implemented the checks for the gamemode switcher

--- src/main/java/com/github/barnabeepickle/mgms/core/mixins/early/minecraft/MinecraftKeyF3Mixin.java
+++ src/main/java/com/github/barnabeepickle/mgms/core/mixins/early/minecraft/MinecraftKeyF3Mixin.java
@@ -12,9 +12,22 @@ public class MinecraftKeyF3Mixin {
 
     @Inject(method = "processKeyF3(I)Z", at = @At("RETURN"))
     private void processKeyF3(int auxKey, CallbackInfoReturnable<Boolean> cir) {
+        // Check if the return value is false so we know no other F3 + keybinds are acting
         if (!cir.getReturnValue()) {
+            Minecraft minecraft = Minecraft.getMinecraft();
+            // Check if auxKey is the keybind's key code for the switcher
             if (auxKey == ModernSwitcherMod.switcherKeybind.getKeyCode()) {
-
+                // Run a check that the player isn't borked
+                if (ModernSwitcherMod.canSwitchGameMode(minecraft)) {
+                    // Run a check that the player has permissions to switch gamemodes
+                    if (minecraft.player.canUseCommand(2, "")) {
+                        // This is where the switcher ui is opened
+                    } else {
+                        minecraft.debugFeedbackTranslated("debug.mgms.switcher.error.permissions");
+                    }
+                } else {
+                    minecraft.debugFeedbackTranslated("debug.mgms.switcher.error.unable");
+                }
             }
         }
     }
