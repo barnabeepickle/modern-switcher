654bd6faf344443d0ba0ff41596a3d37f4d1126c
barnabeepickle <picklebarnabee@gmail.com>
2026-03-31 11:19:22 -0400

    added coremod

---
+++ src/main/java/com/github/barnabeepickle/mgms/core/ModernSwitcherCoreMod.java
@@ -0,0 +1,42 @@
+package com.github.barnabeepickle.mgms.core;
+
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+import zone.rong.mixinbooter.IEarlyMixinLoader;
+
+import javax.annotation.Nullable;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+public class ModernSwitcherCoreMod implements IFMLLoadingPlugin, IEarlyMixinLoader {
+    @Override
+    public String[] getASMTransformerClass() {
+        return null;
+    }
+
+    @Override
+    public String getModContainerClass() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getSetupClass() {
+        return null;
+    }
+
+    @Override
+    public void injectData(Map<String, Object> data) {
+
+    }
+
+    @Override
+    public String getAccessTransformerClass() {
+        return null;
+    }
+
+    @Override
+    public List<String> getMixinConfigs() {
+        return Collections.singletonList("mixins.mgms.json");
+    }
+}
