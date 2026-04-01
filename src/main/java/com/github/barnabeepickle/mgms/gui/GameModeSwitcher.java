ba1d6832f15fd91076b26f2ee3f31a882eeedce9
barnabeepickle <picklebarnabee@gmail.com>
2026-04-01 10:11:33 -0400

    improved the screen

--- src/main/java/com/github/barnabeepickle/mgms/gui/GameModeSwitcher.java
+++ src/main/java/com/github/barnabeepickle/mgms/gui/GameModeSwitcher.java
@@ -1,5 +1,6 @@
 package com.github.barnabeepickle.mgms.gui;
 
+import com.cleanroommc.modularui.api.IThemeApi;
 import com.cleanroommc.modularui.screen.CustomModularScreen;
 import com.cleanroommc.modularui.screen.ModularPanel;
 import com.cleanroommc.modularui.screen.viewport.ModularGuiContext;
@@ -9,12 +10,13 @@ import javax.annotation.Nonnull;
 public class GameModeSwitcher extends CustomModularScreen {
     public GameModeSwitcher(String owner) {
         super(owner);
+        IThemeApi.get().registerThemeForScreen(owner, "switcher_panel", "vanilla_dark");
     }
 
     @Nonnull
     @Override
     public ModularPanel buildUI(ModularGuiContext context) {
-        ModularPanel panel = ModularPanel.defaultPanel("switcher_panel");
+        ModularPanel panel = ModularPanel.defaultPanel("switcher_panel").size(125, 75);
         return panel;
     }
 }
