2baad94bd69ac491eb1912499c97d3435922ac2d
barnabeepickle <picklebarnabee@gmail.com>
2026-04-01 09:22:01 -0400

    added CustomModularScreen to GameModeSwitcher

--- src/main/java/com/github/barnabeepickle/mgms/gui/GameModeSwitcher.java
+++ src/main/java/com/github/barnabeepickle/mgms/gui/GameModeSwitcher.java
@@ -1,4 +1,17 @@
 package com.github.barnabeepickle.mgms.gui;
 
-public class GameModeSwitcher {
+import com.cleanroommc.modularui.screen.CustomModularScreen;
+import com.cleanroommc.modularui.screen.ModularPanel;
+import com.cleanroommc.modularui.screen.viewport.ModularGuiContext;
+
+import javax.annotation.Nonnull;
+
+@SuppressWarnings({"UnstableApiUsage", "deprecation"})
+public class GameModeSwitcher extends CustomModularScreen {
+    @Nonnull
+    @Override
+    public ModularPanel buildUI(ModularGuiContext context) {
+        ModularPanel panel = ModularPanel.defaultPanel("switcher_panel");
+        return panel;
+    }
 }
