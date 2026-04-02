0dd979e9f14e3b207af6d36ae0f8c2600ed8585a
barnabeepickle <picklebarnabee@gmail.com>
2026-04-02 12:15:50 -0400

    switched to subclassing the correct way

--- src/main/java/com/github/barnabeepickle/mgms/gui/ModeIconWidget.java
+++ src/main/java/com/github/barnabeepickle/mgms/gui/ModeIconWidget.java
@@ -1,12 +1,15 @@
 package com.github.barnabeepickle.mgms.gui;
 
-import com.cleanroommc.modularui.widgets.CycleButtonWidget;
+import com.cleanroommc.modularui.api.drawable.IDrawable;
+import com.cleanroommc.modularui.api.value.IIntValue;
+import com.cleanroommc.modularui.screen.RichTooltip;
+import com.cleanroommc.modularui.widgets.AbstractCycleButtonWidget;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.util.function.Consumer;
 
-public class ModeIconWidget extends CycleButtonWidget {
+public class ModeIconWidget extends AbstractCycleButtonWidget<ModeIconWidget> {
     @Nullable
     private Consumer<ModeIconWidget> onMouseEnterHandler;
 
@@ -51,5 +54,93 @@ public class ModeIconWidget extends CycleButtonWidget {
             this.onMouseEnterHandler = listener;
         }
         return this;
+    }
+
+    // The following is just from CycleButtonWidget
+
+    @Override
+    public ModeIconWidget value(IIntValue<?> value) {
+        return super.value(value);
+    }
+
+    public ModeIconWidget stateBackground(int state, IDrawable drawable) {
+        this.background = addToArray(this.background, drawable, state);
+        return disableThemeBackground(true);
+    }
+
+    public ModeIconWidget stateHoverBackground(int state, IDrawable drawable) {
+        this.hoverBackground = addToArray(this.hoverBackground, drawable, state);
+        return disableHoverThemeBackground(true);
+    }
+
+    public ModeIconWidget stateOverlay(int state, IDrawable drawable) {
+        this.overlay = addToArray(this.overlay, drawable, state);
+        return getThis();
+    }
+
+    public ModeIconWidget stateHoverOverlay(int state, IDrawable drawable) {
+        this.hoverOverlay = addToArray(this.hoverOverlay, drawable, state);
+        return getThis();
+    }
+
+    public ModeIconWidget stateBackground(boolean state, IDrawable drawable) {
+        return stateBackground(state ? 1 : 0, drawable);
+    }
+
+    public ModeIconWidget stateHoverBackground(boolean state, IDrawable drawable) {
+        return stateHoverBackground(state ? 1 : 0, drawable);
+    }
+
+    public ModeIconWidget stateOverlay(boolean state, IDrawable drawable) {
+        return stateOverlay(state ? 1 : 0, drawable);
+    }
+
+    public ModeIconWidget stateHoverOverlay(boolean state, IDrawable drawable) {
+        return stateHoverOverlay(state ? 1 : 0, drawable);
+    }
+
+    public <T extends Enum<T>> ModeIconWidget stateBackground(T state, IDrawable drawable) {
+        return stateBackground(state.ordinal(), drawable);
+    }
+
+    public <T extends Enum<T>> ModeIconWidget stateHoverBackground(T state, IDrawable drawable) {
+        return stateHoverBackground(state.ordinal(), drawable);
+    }
+
+    public <T extends Enum<T>> ModeIconWidget stateOverlay(T state, IDrawable drawable) {
+        return stateOverlay(state.ordinal(), drawable);
+    }
+
+    public <T extends Enum<T>> ModeIconWidget stateHoverOverlay(T state, IDrawable drawable) {
+        return stateHoverOverlay(state.ordinal(), drawable);
+    }
+
+    @Override
+    public ModeIconWidget addTooltip(int state, String tooltip) {
+        return super.addTooltip(state, tooltip);
+    }
+
+    @Override
+    public ModeIconWidget addTooltip(int state, IDrawable tooltip) {
+        return super.addTooltip(state, tooltip);
+    }
+
+    public ModeIconWidget length(int length) {
+        return stateCount(length);
+    }
+
+    @Override
+    public ModeIconWidget stateCount(int stateCount) {
+        return super.stateCount(stateCount);
+    }
+
+    @Override
+    public ModeIconWidget tooltip(int index, Consumer<RichTooltip> builder) {
+        return super.tooltip(index, builder);
+    }
+
+    @Override
+    public ModeIconWidget tooltipBuilder(int index, Consumer<RichTooltip> builder) {
+        return super.tooltipBuilder(index, builder);
     }
 }
