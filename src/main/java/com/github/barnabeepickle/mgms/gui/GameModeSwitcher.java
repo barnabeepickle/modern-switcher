6696e18127aeb6fffc251c43d313adb0f579613d
barnabeepickle <picklebarnabee@gmail.com>
2026-04-17 09:09:44 -0400

    commented out some code

--- src/main/java/com/github/barnabeepickle/mgms/gui/GameModeSwitcher.java
+++ src/main/java/com/github/barnabeepickle/mgms/gui/GameModeSwitcher.java
@@ -218,10 +218,10 @@ public class GameModeSwitcher extends CustomModularScreen {
         panel.child(modeButtons);
 
         panel.onUpdateListener(listener -> {
-            if (!Keyboard.isKeyDown(Keyboard.KEY_F3)) {
-                this.attemptSwitchMode(minecraft);
-                panel.closeIfOpen();
-            }
+            //if (!Keyboard.isKeyDown(Keyboard.KEY_F3)) {
+            //    this.attemptSwitchMode(minecraft);
+            //    panel.closeIfOpen();
+            //}
             if (ModernSwitcherMod.switcherKeybind.isPressed()) {
                 advanceMode();
             }
