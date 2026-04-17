0585c93e545634cf1d2ba06de07a3434c48c0677
barnabeepickle <picklebarnabee@gmail.com>
2026-04-17 09:10:17 -0400

    removed some comments as modularui got updated

--- src/main/java/com/github/barnabeepickle/mgms/gui/GameModeSwitcher.java
+++ src/main/java/com/github/barnabeepickle/mgms/gui/GameModeSwitcher.java
@@ -109,8 +109,6 @@ public class GameModeSwitcher extends CustomModularScreen {
 
         ModularPanel panel = ModularPanel.defaultPanel("switcher_panel").size(125, 75);
 
-        // RichTextWidget is currently broken and won't center the text correctly
-        // this will be fixed in the next modularui update
         RichTextWidget gamemodeText = new RichTextWidget()
                 .horizontalCenter()
                 .fullWidth()
@@ -128,8 +126,6 @@ public class GameModeSwitcher extends CustomModularScreen {
         });
         panel.child(gamemodeText);
 
-        // RichTextWidget is currently broken and won't center the text correctly
-        // this will be fixed in the next modularui update
         RichTextWidget keybindNextText = new RichTextWidget()
                 .horizontalCenter()
                 .fullWidth()
